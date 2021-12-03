6ec17c8642d7f78b8648c9c141b356dcf6fea15b
ibelkhiria <ibelkhiria@elyosdigital.com>
2021-12-03 10:39:50 +0100

    return data with vue

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
+| Here is where you can register API  routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('posts','PostController@index');
